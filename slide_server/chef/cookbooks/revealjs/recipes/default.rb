f2f059f1370f2907c708fc7d8a461f31e466839c
Jonathan Lassam <jlassam@hotmail.com>
2013-10-27 16:21:15 -0700

    got started on slide server

---
+++ slide_server/chef/cookbooks/revealjs/recipes/default.rb
@@ -0,0 +1,24 @@
+
+# install grunt
+
+# npm install -g grunt-cli
+
+# install git
+
+#Clone the reveal.js repository
+
+#$ git clone https://github.com/hakimel/reveal.js.git
+
+#Navigate to the reveal.js folder
+
+#$ cd reveal.js
+
+#Install dependencies
+
+#3$ npm install
+
+#Serve the presentation and monitor source files for changes
+
+#$ grunt serve
+
+#Open http://localhost:8000 to view your presentation
